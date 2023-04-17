5c72ef52466770417686c4f69ebdc5056afc007e
Kirill Filonchuk <kir.realtor@gmail.com>
2023-04-17 14:24:57 +0300

    Updates

--- src/components/Box/Box.jsx
+++
@@ -1,8 +0,0 @@
-import styled from "styled-components";
-import { color, layout, space, flexbox } from "styled-system";
-
-export const Box = styled("div")(color, space, layout, flexbox);
-
-// export const Box = styled.div`
-//   ${color}
-// `;
