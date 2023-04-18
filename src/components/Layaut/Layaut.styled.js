26bf8f7a53b59e827ab3e6973170a8d1ba9a35ed
Kirill Filonchuk <kir.realtor@gmail.com>
2023-04-18 23:55:39 +0300

    add style

--- src/components/Layaut/Layaut.styled.js
+++ src/components/Layaut/Layaut.styled.js
@@ -20,6 +20,9 @@ export const ItemNav = styled.li`
   background-size: 200% auto;
   box-shadow: 0 0 20px rgba(0, 0, 0, 0.1);
   transition: 0.5s;
+  &:hover {
+    background-position: right center;
+  }
 `;
 
 export const NavBarLink = styled(NavLink)`
